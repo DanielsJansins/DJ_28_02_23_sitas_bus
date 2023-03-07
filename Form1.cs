c6c1c8166fb03f8ce068f3f1e5af51a115e03183
Dāniels Jansiņš <dj2782@gymn.local>
2023-03-07 09:51:02 +0200

    nu ir jastrada

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
+
 
 namespace WindowsFormsApp2
 {
@@ -47,6 +49,41 @@ namespace WindowsFormsApp2
         }
 
         private void budgetTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                string buyerName = textbox1.Text;
+                string date = DateTime.Now.ToString("ddMMyyyy");
+
+                string fileName = $"{buyerName}_{date}_ceks.txt";
+                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine($"Pircējs: {buyerName}");
+                    writer.WriteLine($"Datums: {DateTime.Now}");
+                }
+
+                MessageBox.Show($"Dati saglabāti failā: {fileName}");
+            }
+
+                MessageBox.Show($"Dati saglabāti failā: {fileName}");
+            }
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
 
         }
